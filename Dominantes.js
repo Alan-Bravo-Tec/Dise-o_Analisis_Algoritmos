d447f1ee7e6d1c5ded417561a18e1a4fbd3287a3
Alan-Bravo-Tec <142810565+Alan-Bravo-Tec@users.noreply.github.com>
2023-10-26 23:14:44 -0600

    Add files via upload

---
+++ Dominantes.js
@@ -0,0 +1,21 @@
+function Dominantes(A) {
+    let Dom = [];
+    let j = 0;
+
+    for (let i = 0; i < A.length - 1; i++) {
+        if (A[i] > 1000 || A[i] < -100) {
+            console.log("El número en la posición " + i + " es menor a -1000 o mayor que 1000");
+        } else {
+            if (A[i] > A[i + 1]) {
+                Dom[j] = A[i];
+                j++;
+            }
+        }
+    }
+
+    Dom[j] = A[A.length - 1];
+    return Dom;
+}
+
+const A = [1, 21, 4, 7, 5];
+
